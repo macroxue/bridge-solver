3db104ca08e6796de22bf10c213c79a0513f4138
Hanhong Xue <macroxue@hotmail.com>
2021-03-29 20:37:52 -0400

    Add a script to run tests in parallel and check results

---
+++ parallel_run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $1 == '-r' ]]; then
+  echo $3 $(./solver -fi $2/$3 | sed -e "s/[0-9]*\.[0-9]* [sM]//g")
+  exit
+fi
+
+test_dir=${1:-old_deals}
+test_dir=${test_dir%%/*}  # remove trailing slashes
+parallelism=${2:-2}
+results=results.$test_dir.$parallelism
+echo Results are in $results
+
+start=$(date +"%s.%N")
+ls $test_dir -I RESULTS | \
+  xargs -L 1 -P $parallelism ./parallel_run_tests.sh -r $test_dir > $results
+finish=$(date +"%s.%N")
+
+num_deals=$(ls $test_dir -I RESULTS | wc -l)
+echo Solved $num_deals deals in $(echo "scale=1;($finish-$start)/1" | bc) seconds
+
+diff <(sed -e "N;N;N;N;N;s/\n/ /g;s/  */ /g" $test_dir/RESULTS | sort) <(sort $results)
--- run_tests.sh
+++ run_tests.sh
@@ -2,20 +2,17 @@
 
 test_dir=${1:-old_deals}
 test_dir=${test_dir%%/*}  # remove trailing slashes
+results=results.$test_dir
+echo Results are in $results
 
-num_deals=0
 start=$(date +"%s.%N")
-for deal in $(ls $test_dir); do
-  if [[ $deal = "RESULTS" ]]; then
-    continue
-  fi
-  num_deals=$((num_deals+1))
+for deal in $(ls $test_dir -I RESULTS); do
   echo $deal
   ./solver -fi $test_dir/$deal
-done > results.$test_dir
+done > $results
 finish=$(date +"%s.%N")
 
-echo Solved $num_deals deals in $(echo "scale=1;($finish-$start)/1" | bc) seconds.
-echo Results are in \"results.$test_dir\".
+num_deals=$(ls $test_dir -I RESULTS | wc -l)
+echo Solved $num_deals deals in $(echo "scale=1;($finish-$start)/1" | bc) seconds
 
-diff $test_dir/RESULTS <(cut -c1-13 results.$test_dir)
+diff $test_dir/RESULTS <(cut -c1-13 $results)
