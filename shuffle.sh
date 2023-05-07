36fe248c304c2b9762921a1efcad92215b0b6f02
Hanhong Xue <macroxue@hotmail.com>
2023-05-07 10:56:36 -0400

    An option to show standard error with shuffle.sh

--- shuffle.sh
+++ shuffle.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 code=
+show_err=0
 rounds=20
-while getopts c:r: flag
+while getopts c:er: flag
 do
   case $flag in
     c) code=$OPTARG;;
+    e) show_err=1;;
     r) rounds=$OPTARG;;
   esac
 done
@@ -25,6 +27,12 @@ for seats in EW NS; do
     north_sum=0
     west_sum=0
     east_sum=0
+    if [[ $show_err -eq 1 ]]; then
+      south_sum2=0
+      north_sum2=0
+      west_sum2=0
+      east_sum2=0
+    fi
     for round in $(seq 1 $rounds); do
       if [[ -t 1 ]]; then
         printf "$round"
@@ -34,6 +42,12 @@ for seats in EW NS; do
       north_sum=$((north_sum+${result[2]}))
       west_sum=$((west_sum+${result[3]}))
       east_sum=$((east_sum+${result[4]}))
+      if [[ $show_err -eq 1 ]]; then
+        south_sum2=$((south_sum2+${result[1]}*${result[1]}))
+        north_sum2=$((north_sum2+${result[2]}*${result[2]}))
+        west_sum2=$((west_sum2+${result[3]}*${result[3]}))
+        east_sum2=$((east_sum2+${result[4]}*${result[4]}))
+      fi
       if [[ -t 1 ]]; then
         printf "$eraser"
       fi
@@ -43,12 +57,26 @@ for seats in EW NS; do
     north_avg=$(echo "scale=1;$north_sum/$rounds" | bc)
     west_avg=$(echo "scale=1;$west_sum/$rounds" | bc)
     east_avg=$(echo "scale=1;$east_sum/$rounds" | bc)
-    if [[ $seats = EW ]]; then
-      printf "$trump  %4.1f %4.1f (%4.1f %4.1f)\n" \
-        $south_avg $north_avg $west_avg $east_avg
+    if [[ $show_err -eq 1 ]]; then
+      south_err=$(echo "scale=1;($south_sum2-$rounds*$south_avg*$south_avg)/($rounds-1)" | bc)
+      north_err=$(echo "scale=1;($north_sum2-$rounds*$north_avg*$north_avg)/($rounds-1)" | bc)
+      west_err=$(echo "scale=1;($west_sum2-$rounds*$west_avg*$west_avg)/($rounds-1)" | bc)
+      east_err=$(echo "scale=1;($east_sum2-$rounds*$east_avg*$east_avg)/($rounds-1)" | bc)
+      if [[ $seats = EW ]]; then
+        printf "$trump  %4.1f±%3.1f %4.1f±%3.1f (%4.1f±%3.1f %4.1f±%3.1f)\n" \
+          $south_avg $south_err $north_avg $north_err $west_avg $west_err $east_avg $east_err
+      else
+        printf "$trump (%4.1f±%3.1f %4.1f±%3.1f) %4.1f±%3.1f %4.1f±%3.1f\n" \
+          $south_avg $south_err $north_avg $north_err $west_avg $west_err $east_avg $east_err
+      fi
     else
-      printf "$trump (%4.1f %4.1f) %4.1f %4.1f\n" \
-        $south_avg $north_avg $west_avg $east_avg
+      if [[ $seats = EW ]]; then
+        printf "$trump  %4.1f %4.1f (%4.1f %4.1f)\n" \
+          $south_avg $north_avg $west_avg $east_avg
+      else
+        printf "$trump (%4.1f %4.1f) %4.1f %4.1f\n" \
+          $south_avg $north_avg $west_avg $east_avg
+      fi
     fi
   done
 done
